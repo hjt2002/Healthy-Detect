d231cbd315dea2d27ec2090f28d3a1d9a07fc8d4
hjt <1161297340@qq.com>
2023-07-05 10:32:51 +0800

    grpc框架搭建

---
+++ src/main/java/com/example/healthdetect/HealthDetectApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthdetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDetectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthDetectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/healthdetect/grpclient/GRPCClient.java
@@ -0,0 +1,70 @@
+package com.example.healthdetect.grpclient;
+
+
+import com.example.grpc.GreeterGrpc;
+import com.example.grpc.HelloReply;
+import com.example.grpc.HelloRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ * A simple client that requests a greeting from the {@link com.example.healthdetect.grpcserver.GRPCServer}.
+ */
+public class GRPCClient {
+    private static final Logger logger = Logger.getLogger(GRPCClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /**
+     * Construct client connecting to HelloWorld server at {@code host:port}.
+     */
+    public GRPCClient(String host, int port) {
+
+        channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext()
+                .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Say hello to server.
+     */
+    public void greet(String name) {
+        logger.info("Will try to greet server " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        // 输出信息
+        logger.info("Greeting from server: " + response.getMessage());
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        GRPCClient client = new GRPCClient("localhost", 50051);
+        try {
+            String user = "hjt";
+            if (args.length > 0) {
+                user = args[0];
+            }
+            client.greet(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
